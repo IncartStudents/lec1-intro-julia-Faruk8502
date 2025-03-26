18d0c290f572c688627a9be807c89b042a9d0f62
Faruk <90871155+Faruk8502@users.noreply.github.com>
2025-03-26 13:20:13 +0300

    Update level4_boids.jl

--- level4_boids.jl
+++ level4_boids.jl
@@ -1,36 +1,72 @@
 module Boids
 using Plots
+using Random
 
+mutable struct Boid
+    x::Float64
+    y::Float64
+    vx::Float64
+    vy::Float64
+end
 
 mutable struct WorldState
-    boid::Tuple{Float64, Float64}
+    boids::Vector{Boid}
     height::Float64
     width::Float64
     function WorldState(n_boids, width, height)
-        # TODO: добавить случайные позиции для n_boids птичек вместо одной
+        boids = Vector{Boid}()
+        rng = MersenneTwister(1234) # Для воспроизводимости
         
-        new((width/2, height/2), width, height)
+        for _ in 1:n_boids
+            x = rand(rng) * width
+            y = rand(rng) * height
+            vx = randn(rng) * 0.1
+            vy = randn(rng) * 0.1
+            push!(boids, Boid(x, y, vx, vy))
+        end
+        
+        new(boids, width, height)
     end
 end
 
 function update!(state::WorldState)
-    state.boid = state.boid .+ 0.1
-    # TODO: реализация уравнения движения птичек
-
+    for boid in state.boids
+        # Обновляем позицию
+        boid.x += boid.vx
+        boid.y += boid.vy
+        
+        # Обработка границ - если boid выходит за границы, появляется с другой стороны
+        if boid.x < 0
+            boid.x = state.width
+        elseif boid.x > state.width
+            boid.x = 0
+        end
+        
+        if boid.y < 0
+            boid.y = state.height
+        elseif boid.y > state.height
+            boid.y = 0
+        end
+    end
     return nothing
 end
 
-function (@main)(ARGS)
-    w = 30
-    h = 30
+function main(ARGS)
+    w = 30.0
+    h = 30.0
     n_boids = 10
 
     state = WorldState(n_boids, w, h)
 
     anim = @animate for time = 1:100
         update!(state)
-        boids = [state.boid]
-        scatter(boids, xlim = (0, state.width), ylim = (0, state.height))
+        scatter([(b.x, b.y) for b in state.boids], 
+                xlim = (0, state.width), 
+                ylim = (0, state.height),
+                legend = false,
+                title = "Boids Simulation",
+                markersize = 5,
+                markercolor = :blue)
     end
     gif(anim, "boids.gif", fps = 10)
 end
